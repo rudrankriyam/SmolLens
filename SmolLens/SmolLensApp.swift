2bb6a1ab70bdd3b46229c9f9d88616e9e35f381c
Rudrank Riyam <rudrankriyam@gmail.com>
2025-02-20 22:26:45 +0530

    Initial commit with MLX Swift package

---
+++ SmolLens/SmolLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmolLensApp.swift
+//  SmolLens
+//
+//  Created by Rudrank Riyam on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SmolLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
